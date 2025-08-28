abf3a347dc30f1b321a34aef5d351c8e81ad0a68
zderApfel <67031028+zderApfel@users.noreply.github.com>
2025-08-28 15:52:29 -0400

    Further inventory tweeks

--- ui/scripts/inventory_entry.gd
+++ ui/scripts/inventory_entry.gd
@@ -3,10 +3,15 @@ class_name InventoryEntry extends Button
 ## The item stored in the entry. Has to be lowercase due to my dumb naming conventions
 @onready var item: Item
 
+
+func _physics_process(delta) -> void:
+	pass
+
 func activate(x: Item) -> void:
 	item = x
 	self.text = item.Item_Name
 	if item.Inv_Icon != null: $Icon.texture = load(item.Inv_Icon)
 	
-func _on_pressed() -> void:
-	print(item.Item_Name)
+func _on_gui_input(event: InputEvent) -> void:
+	if event.is_action_released("primary_action"):
+		print(item.Item_Name)
